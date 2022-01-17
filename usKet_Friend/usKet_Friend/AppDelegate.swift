1ebd835696022d9fac9dea1f0a382a695b237ffa
9oHigh <wkdska957@naver.com>
2022-01-17 17:10:30 +0900

    PhoneNumber Auth

--- usKet_Friend/usKet_Friend/AppDelegate.swift
+++ usKet_Friend/usKet_Friend/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Firebase
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in }
+        
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
--- usKet_Friend/usKet_Friend/ViewController.swift
+++ usKet_Friend/usKet_Friend/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SnapKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    var checkButton = UIButton()
+    var phoneNumberTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
+        view.addSubview(checkButton)
+        view.addSubview(phoneNumberTextField)
+        
+        checkButton.backgroundColor = .black
+        checkButton.tintColor = .white
+        checkButton.setTitle("인증", for: .normal)
+        
+        phoneNumberTextField.backgroundColor = .systemGray6
+        phoneNumberTextField.borderStyle = .roundedRect
+        phoneNumberTextField.placeholder = "전화번호 입력"
+        
+        phoneNumberTextField.snp.makeConstraints { make in
+            make.leading.equalTo(20)
+            make.trailing.equalTo(20)
+            make.top.equalTo(100)
+            make.bottom.equalTo(checkButton.snp.top).offset(-30)
+        }
+        
+        checkButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.width.equalTo(150)
+            make.top.equalTo(phoneNumberTextField.snp.bottom)
+        }
+        
+        checkButton.addTarget(self, action: #selector(sendVarification), for: .touchUpInside)
+    }
+    
+    @objc func sendVarification(){
+        
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumberTextField.text ?? "", uiDelegate: nil) { varification, error in
+            if error == nil {
+                print("SUCCESS")
+            } else {
+                print("FAILED")
+            }
+            
+        }
+        
     }
 
-
+    
 }
 
