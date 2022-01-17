56ad6a25b191bc4db82be0c16a9206bead4da4b5
9oHigh <wkdska957@naver.com>
2022-01-17 15:49:39 +0900

    init

---
+++ usKet_Friend/usKet_Friend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usKet_Friend
+//
+//  Created by 이경후 on 2022/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
